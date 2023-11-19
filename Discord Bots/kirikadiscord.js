8fd33354b2db1f105d1f548f5a8689fdd3441e5e
ShadowBeatz <shadowbeatzinc@gmail.com>
2023-11-19 04:52:20 -0500

    cleaned up the code a bit

--- Discord Bots/kirikadiscord.js
+++ Discord Bots/kirikadiscord.js
@@ -76,73 +76,6 @@ client.on("messageCreate", message => {
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
 
-    async function getTeamInfo(sport, league, abbr) {
-        try {
-            const req = await fetch('http://site.api.espn.com/apis/site/v2/sports/' + sport + '/' + league + '/teams/' + abbr, {
-                method: 'get',
-                headers: {},
-                redirect: 'follow'
-            });
-            //sends the request
-            espn = await req.json()
-                              //formats the raw request into JSON
-
-            if (espn.team != undefined) {
-                var M = league 
-                let teaminfo = []
-
-                if (espn.team.color === '000000') {
-                    teaminfo.push(espn.team.displayName, espn.team.logos[1].href, espn.team.alternateColor, espn.team.record.items[0].summary, espn.team.standingSummary)
-                } else {
-                    teaminfo.push(espn.team.displayName, espn.team.logos[1].href, espn.team.color, espn.team.record.items[0].summary, espn.team.standingSummary)
-                }
-
-                if (espn.team.displayName.includes(' Eagles') || espn.team.displayName.includes(' Cowboys') || espn.team.displayName.includes(' Flyers') || espn.team.displayName.includes(' Phillies') || espn.team.displayName.includes(' Braves') || espn.team.displayName.includes(' Nationals') || espn.team.displayName.includes(' Yankees') || espn.team.displayName.includes(' Capitals')) {
-                    teaminfo.push('<:beatzSusAF:549413960948908063>')
-                } else {
-                    if (espn.team.standingSummary[0] === '1') {
-                        teaminfo.push('<:beatzWICKED:1165575471153549342>')
-                    } else {
-                        if (espn.team.record.items[0].stats[4].name === 'clincher') {
-                            if (espn.team.record.items[0].stats[18].value < 0.5) {
-                                teaminfo.push('<:beatzDespair:1019839939522859109>')
-                            } else {
-                                teaminfo.push('<:KirikaSmile:608201680374464532>')
-                            }
-                        } else if (espn.team.record.items[0].stats[0].name === 'otLosses') {
-                            if (espn.team.record.items[0].stats[15].value < 0.5) {
-                                teaminfo.push('<:beatzDespair:1019839939522859109>')
-                            } else {
-                                teaminfo.push('<:KirikaSmile:608201680374464532>')
-                            }
-                        } else {
-                            if (espn.team.record.items[0].stats[17].value < 0.5) {
-                                teaminfo.push('<:beatzDespair:1019839939522859109>')
-                            } else {
-                                teaminfo.push('<:KirikaSmile:608201680374464532>')
-                            }
-                        }
-                    }
-                }
-
-                if (espn.team.nextEvent[0] != undefined) {
-                    if ((new Date().getTime()) - (Date.parse(espn.team.nextEvent[0].date)) < 0) {
-                        teaminfo.push('\n\nTheir next game is: \n' + espn.team.nextEvent[0].competitions[0].competitors[1].team.shortDisplayName + ' vs. ' + espn.team.nextEvent[0].competitions[0].competitors[0].team.shortDisplayName + ' (' + espn.team.nextEvent[0].competitions[0].status.type.shortDetail + ')\n\n')
-                    }
-
-
-                }
-
-                return (teaminfo)
-            } else {
-                return Promise.reject('getTeamInfo() Kirika Promise Error')
-            }
-        } catch (err) {
-            message.channel.send('Hang on. ESPN is being a baka <:beatzBaka:1167640027652698312> Try again in a second')
-            console.log('getTeamInfo\n' + err)
-        }
-    }
-
     function TitleCase(Input) {
         Input = Input.toLowerCase().split(" ");
 
@@ -167,77 +100,6 @@ client.on("messageCreate", message => {
         } else {
             return ("a mystery person")
         }
-    }
-
-    function getTeams(id) {
-        fs.readFile('sports.json', 'utf8', (err, data) => {
-            sports = JSON.parse(data)
-
-
-            let LeaguesEntries = Object.entries(sports)
-            userfandom = []
-            SPORT = undefined
-            LEAGUE = undefined
-
-
-            for (let i = 0; i < LeaguesEntries.length; i++) {
-                let TeamsEntries = Object.entries(LeaguesEntries[i][1])
-                for (let j = 0; j < TeamsEntries.length; j++) {
-                    let FansEntries = Object.entries(TeamsEntries[j][1])
-                    if (FansEntries[0][1].includes(id)) {
-                        if (LeaguesEntries[i][0].toLowerCase() === 'nfl') {
-                            SPORT = 'football'
-                            LEAGUE = 'nfl'
-                        }
-                        if (LeaguesEntries[i][0].toLowerCase() === 'nhl') {
-                            SPORT = 'hockey'
-                            LEAGUE = 'nhl'
-                        }
-                        if (LeaguesEntries[i][0].toLowerCase() === 'nba') {
-                            SPORT = 'basketball'
-                            LEAGUE = 'nba'
-                        }
-                        if (LeaguesEntries[i][0].toLowerCase() === 'mlb') {
-                            SPORT = 'baseball'
-                            LEAGUE = 'mlb'
-                        }
-                        if (LeaguesEntries[i][0].toLowerCase() === 'ncaaf') {
-                            SPORT = 'football'
-                            LEAGUE = 'college-football'
-                        }
-                        if (LeaguesEntries[i][0].toLowerCase() === 'wnba') {
-                            SPORT = 'basketball'
-                            LEAGUE = 'wnba'
-                        }
-                        userfandom.push([LeaguesEntries[i][0].toLowerCase(), TeamsEntries[j][0], SPORT, TeamsEntries[j][1].abbr])
-                    }
-                }
-            }
-
-            if (userfandom.length < 1) {
-                message.channel.send('' + username(id) + ' hasn\'t told me who their favorite teams are yet <:beatzDespair:1019839939522859109> Maybe they don\'t like sports')
-            } else {
-                teamArray()
-
-                async function teamArray() {
-                    var teamarray = []
-                    for (var k = 0; k < userfandom.length; k++) {
-                        const a = await getTeamInfo(userfandom[k][2], userfandom[k][0], userfandom[k][3])
-                        teamarray.push([a[0], ' --- ' + a[3] + ' ', '(' + a[4] + ') ' + a[5]] + '\n')
-                    }
-
-                    const teamsEmbed = new EmbedBuilder()
-                        .setColor(8446019)
-                        .setTitle(username(id) + '\'s Favorite Teams')
-                        .setThumbnail(client.guilds.cache.get('172065393525915648').members.cache.get(id).user.displayAvatarURL())
-                        .addFields(
-                            { name: '=======================================', value: teamarray.join().replaceAll(',', '') }
-                        )
-
-                    message.channel.send({ embeds: [teamsEmbed] })
-                }
-            }
-        })
     }
 
     if (message.channel.type != undefined && message.author != null) {
@@ -840,4 +702,29 @@ schedule.scheduleJob('0 1 0 * * *', function () {
         client.guilds.cache.get('172065393525915648').members.cache.get(tts).roles.add('762186129918394399');
         client.channels.cache.get('607815006633066496').send('' + Weeb + ' (ID: ' + tts + ') is the weeb of the day <:KirikaSmile:608201680374464532>')
     }
+});
+
+schedule.scheduleJob('0 0 2 * * *', function () {
+    fs.readFile('kirikapredictions.json', 'utf8', (err, data) => {
+        scores = JSON.parse(data)
+        let x = 0
+        let total = 0
+
+            if (scores.kirika != undefined) {
+                for (i = scores.kirika.prediction.length - 1; i >= 0; i--) {
+                    total++
+                    if (((new Date().getTime()) - (Date.parse(scores.kirika.prediction[i][6]))) > 0) {
+                        scores.kirika.prediction.splice(i, 1)
+                        x++
+                    }
+                }
+
+                fs.writeFile('kirikapredictions.json', JSON.stringify(scores), (err) => {
+                    var d = new Date();
+                    var time = (d.getHours()).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + ':' + (d.getMinutes().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })) + ':' + (d.getSeconds()).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + ' -'
+                    if (err) throw err;
+                    console.log(`${time} Removed ${x} of ${total} predictions :O`)
+                })
+            }
+    })
 });
